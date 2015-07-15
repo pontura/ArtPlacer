0515f5ae66046c88febe3a6e775bfae8267e89ea
jpamato <juanpamato@gmail.com>
2015-07-15 16:38:08 -0300

    add area to wall

---
+++ Assets/WallPlane.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallPlane : MonoBehaviour {
+
+	public float width = 1000f;
+	public float height = 1000f;
+
+	public float zPointers = -10;
+
+	public GameObject[] pointer;
+	public GameObject area;
+	
+	public GameObject planoContenedor;
+	
+	int select = -1;
+
+	LineRenderer lineRenderer;
+		
+	// Use this for initialization
+	void Start () {
+
+		area.name = "Area_" + gameObject.GetInstanceID ();
+
+
+		MeshFilter mf = GameObject.Find("Area_" + gameObject.GetInstanceID ()).GetComponent<MeshFilter> ();
+
+		for(int i=0;i<mf.mesh.vertexCount;i++){
+			Vector3 vertexWorldPos = area.transform.TransformPoint(mf.mesh.vertices[i]);
+			vertexWorldPos = new Vector3(vertexWorldPos.x,vertexWorldPos.y,zPointers);
+
+			//Instantiate(pointer[i], vertexWorldPos, Quaternion.identity);
+			//pointer[i] = GameObject.CreatePrimitive(PrimitiveType.Quad);
+			pointer[i].transform.position = vertexWorldPos;
+			//pointer[i].transform.localScale += new Vector3(200F, 200F, 0);
+			//pointer[i].tag = "Pointer"+i;
+			//pointer[i].transform.parent = transform;
+			Debug.Log ("Punto"+i+": "+pointer[i].transform.position);
+			pointer[i].GetComponent<Renderer>().material.color = Color.blue;
+			pointer[i].name = "Pointer_"+gameObject.GetInstanceID()+"_"+i;
+
+		}	
+
+		LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
+		//lineRenderer.SetColors(Color.blue,Color.blue);
+		//lineRenderer.SetWidth(50F, 50F);
+
+		RedrawLine ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+					
+		if( Input.GetButton ("Fire1")) {
+			Vector3 mousePos = new Vector3(Input.mousePosition.x,Input.mousePosition.y,0);
+			Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+			
+			Ray screenRay = Camera.main.ScreenPointToRay(mousePos);
+			RaycastHit rayhit;
+			if(Physics.Raycast(screenRay, out rayhit)){
+				Mesh mesh = GameObject.Find("Area_" + gameObject.GetInstanceID ()).GetComponent<MeshFilter> ().mesh;
+				
+				if(select<0){
+					print ("Mesh Id Selected: "+mesh.GetInstanceID());
+					for(int i=0;i<mesh.vertexCount;i++){
+						if(rayhit.collider.name == "Pointer_"+gameObject.GetInstanceID()+"_"+i){ 
+							//Debug.Log ("Pointer Sel: "+i);
+							
+							select= i;
+							//GameObject localArea = GameObject.Find("Area_" + gameObject.GetInstanceID ());
+							Vector3 pos = screenRay.GetPoint(rayhit.distance);							
+							Vector3 areaPos = area.transform.InverseTransformPoint(pos);
+							
+							Vector3[] vertex = new Vector3[4];
+							vertex = mesh.vertices;														
+							
+							vertex[select] = areaPos;
+							mesh.vertices = vertex;
+							//pointer[select].transform.position = new Vector3(pos.x,pos.y,-0.001f);
+							GameObject ponter = GameObject.Find("Area_" + gameObject.GetInstanceID ());
+							rayhit.collider.transform.position = new Vector3(pos.x,pos.y,zPointers);						
+							
+						}
+					}
+				}else{
+					if(rayhit.collider.name == "Pointer_"+gameObject.GetInstanceID()+"_"+select){ 
+
+						//Debug.Log ("Pointer Move: "+select);
+						Vector3 pos = screenRay.GetPoint(rayhit.distance);						
+						Vector3 areaPos = area.transform.InverseTransformPoint(pos);					
+						
+						Vector3[] vertex = new Vector3[4];
+						vertex = mesh.vertices;					
+						vertex[select] = areaPos;						
+						mesh.vertices = vertex;
+						rayhit.collider.transform.position = new Vector3(pos.x,pos.y,zPointers);
+						RedrawLine();					
+					}
+				}
+			}
+		}else{
+			if(select>-1){
+				select=-1;
+				//Debug.Log ("Pointer Reset");
+			}
+		}
+
+
+		if (transform.hasChanged){
+			RedrawLine();
+		}
+	}
+
+	void RedrawLine(){
+		int lineSize1 = (int)Vector3.Distance(pointer[0].transform.position,pointer[2].transform.position);
+		int lineSize2 = (int)Vector3.Distance(pointer[2].transform.position,pointer[1].transform.position);
+		int lineSize3 = (int)Vector3.Distance(pointer[1].transform.position,pointer[3].transform.position);
+		int lineSize4 = (int)Vector3.Distance(pointer[3].transform.position,pointer[0].transform.position);
+		int totalLineS = lineSize1 + lineSize2 + lineSize3 + lineSize4;
+		LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
+		lineRenderer.SetVertexCount(totalLineS);
+		for(int j=0;j<totalLineS;j++){
+			if(j<lineSize1){
+				lineRenderer.SetPosition(j, Vector3.Lerp(pointer[0].transform.position,pointer[2].transform.position,1f*j/lineSize1));
+			}else if(j<lineSize1+lineSize2){
+				lineRenderer.SetPosition(j, Vector3.Lerp(pointer[2].transform.position,pointer[1].transform.position,1f*(j-lineSize1)/lineSize2));		
+			}else if(j<lineSize1+lineSize2+lineSize3){
+				lineRenderer.SetPosition(j, Vector3.Lerp(pointer[1].transform.position,pointer[3].transform.position,1f*(j-lineSize1-lineSize2)/lineSize3));		
+			}else if(j<lineSize1+lineSize2+lineSize3+lineSize4){
+				lineRenderer.SetPosition(j, Vector3.Lerp(pointer[3].transform.position,pointer[0].transform.position,1f*(j-lineSize1-lineSize2-lineSize4)/lineSize4));		
+			}
+		}
+	}
+}
