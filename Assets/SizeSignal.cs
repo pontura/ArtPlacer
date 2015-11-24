8f7810437820f2bdf95e682cb5b784fa92055429
jpamato <juanpamato@gmail.com>
2015-11-24 16:28:50 -0300

    fix para usar 2 digitos en la altura de la pared

--- Assets/SizeSignal.cs
+++ Assets/SizeSignal.cs
@@ -6,6 +6,7 @@ public class SizeSignal : MonoBehaviour {
 
     public int id;
 
+	public GameObject cursor0;
     public GameObject cursor1;
     public GameObject cursor2;
     public GameObject cursor3;
@@ -13,7 +14,8 @@ public class SizeSignal : MonoBehaviour {
     public Text inputField;
     public Text desc;
 
-    private int height = 2;
+	private int height0 = 0;
+    private int height1 = 2;
     private int height2 = 0;
     private int height3 = 0;
 
@@ -29,21 +31,23 @@ public class SizeSignal : MonoBehaviour {
     public void Init(float _width , float _height)
     {
 		if (Data.Instance.unidad == Data.UnitSys.CM) {
-			height = (int)(_height * 0.01);
-			height2 = (int)(_height * 0.1 - (height * 10));
-			height3 = (int)(_height - (height * 100) - (height2 * 10));
+			height0 = (int)(_height * 0.001);
+			height1 = (int)(_height * 0.01 - (height0 * 10));
+			height2 = (int)(_height * 0.1 - (height0 * 100) - (height1 * 10));
+			height3 = (int)(_height - (height0 * 1000) - (height1 * 100)- (height2 * 10));
 		} else if (Data.Instance.unidad == Data.UnitSys.INCHES) {
 			float inches = CustomMath.cm2inches(_height);
 			float feet = CustomMath.inches2feet(inches);
-			height = (int)feet;
-			height2 = (int)((feet - height)*10);
-			height3 = (int)(((feet - height)*10-height2)*10);
+			height0 = (int)(feet*0.1);
+			height1 = (int)(feet - (height0*10));
+			height2 = (int)((feet - (height0*10+height1))*10);
+			height3 = (int)((feet - (height0*10+height1+height2*0.1))*10);
 		}
         RefreshField();
 	}
     public int GetHeight()
     {
-		string result = "" + (height + "" + height2 + "" + height3);
+		string result = "" + (height0 + "" + height1 + "" + height2 + "" + height3);
 		int resu = 0;
 		if (Data.Instance.unidad == Data.UnitSys.CM) {
 			resu = int.Parse(result);
@@ -72,11 +76,12 @@ public class SizeSignal : MonoBehaviour {
     {
         switch (activeNum)
         {
-            case 0: height = int.Parse(key); break;
-            case 1: height2 = int.Parse(key); break;
-            case 2: height3 = int.Parse(key); break;
+            case 0: height0 = int.Parse(key); break;
+            case 1: height1 = int.Parse(key); break;
+            case 2: height2 = int.Parse(key); break;
+			case 3: height3 = int.Parse(key); break;
         }
-        if (activeNum < 2)
+        if (activeNum < 3)
         activeNum++;
         RefreshField();
     }
@@ -87,7 +92,8 @@ public class SizeSignal : MonoBehaviour {
     }
     void Delete()
     {
-        height = 0;
+		height0 = 0;
+        height1 = 0;
         height2 = 0;
         height3 = 0;
         activeNum = 0;
@@ -99,29 +105,31 @@ public class SizeSignal : MonoBehaviour {
     }
     void RefreshField()
     {
-        inputField.text = height + "." + height2 + height3;
+		inputField.text = ""+height0 + height1 + "." + height2 + height3;
 		if (Data.Instance.unidad == Data.UnitSys.CM) {
-			desc.text = height + " meters, " + height2 + "" + height3 + " centimeters";
+			desc.text = ""+height0 + height1 + " meters, " + height2 + "" + height3 + " centimeters";
 		} else if (Data.Instance.unidad == Data.UnitSys.INCHES) {
-			desc.text = height + " feet, " + height2 + "" + height3 + " inches";
+			desc.text = ""+height0+height1 + " feet, " + height2 + "" + height3 + " inches";
 		}
         
         RefreshCursor();
     }
     void RefreshCursor()
     {
+		cursor0.SetActive(false);
         cursor1.SetActive(false);
         cursor2.SetActive(false);
         cursor3.SetActive(false);
 
-        GameObject cursorActive = cursor1;
-        if (activeNum == 1) cursorActive = cursor2;
-        else if (activeNum == 2) cursorActive = cursor3;
+        GameObject cursorActive = cursor0;
+        if (activeNum == 1) cursorActive = cursor1;
+        else if (activeNum == 2) cursorActive = cursor2;
+		else if (activeNum == 3) cursorActive = cursor3;
         cursorActive.SetActive(true);
     }
     
 	void ConvertUnits(){
-		string result = "" + (height + "" + height2 + "" + height3);
+		string result = "" + (height0 + ""+height1 + "" + height2 + "" + height3);
 		if (Data.Instance.unidad == Data.UnitSys.INCHES) {
 			Init(0,int.Parse(result));
 		} else if (Data.Instance.unidad == Data.UnitSys.CM) {
